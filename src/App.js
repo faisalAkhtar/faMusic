dd1fa2090aef97c8a547aee2627af430297163b5
Faisal Vostro - Ubuntu <faisalakhtar1999@gmail.com>
2021-05-03 20:21:26 +0530

    features - dark mode - material ui on player

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import useAuthtoken from './utils/useAuthtoken'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Player from './components/Player/Player'
 import Login from './components/Login/Login'
-import useAuthtoken from './utils/useAuthtoken'
 
 export default function App() {
 	const { authtoken, setAuthtoken } = useAuthtoken()
+	const theme = createTheme({
+		palette: {
+			mode: 'dark'
+		},
+		typography: {
+			fontFamily: [
+				'Quicksand'
+			].join(',')
+		}
+	})
 
 	if (!authtoken)
-		return <Login setAuthtoken={setAuthtoken} />
+		return (
+			<ThemeProvider theme={theme}>
+				<Login theme={theme} setAuthtoken={setAuthtoken} />
+			</ThemeProvider>
+		)
 
-	return <Player setAuthtoken={setAuthtoken} />
+	return (
+		<ThemeProvider theme={theme}>
+			<Player theme={theme} setAuthtoken={setAuthtoken} />
+		</ThemeProvider>
+	)
 }
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,47 +1,74 @@
 import { useEffect, useRef, useState } from 'react'
-import songslist from '../../songsList.json'
 import PlayerControls from './PlayerControls'
 import PlayerDetails from './PlayerDetails'
+import songslist from '../../songsList.json'
+import { Button, CssBaseline, Grid, Typography } from '@mui/material'
 
 export default function Player({ setAuthtoken }) {
-    const audioEl = useRef(null);
-    const [songs] = useState(songslist);
-    const [currentSongIndex, setCurrentSongIndex] = useState(0);
-    const [nextSongIndex, setnextSongIndex] = useState(currentSongIndex + 1);
-    const [isplaying, setIsplaying] = useState(false);
+    const audioEl = useRef(null)
+    const [songs] = useState(songslist)
+    const [currentSongIndex, setCurrentSongIndex] = useState(0)
+    const [nextSongIndex, setnextSongIndex] = useState(currentSongIndex + 1)
+    const [isplaying, setIsplaying] = useState(false)
 
     useEffect(() => {
-        setnextSongIndex((currentSongIndex + 1 > songs.length - 1) ? 0 : currentSongIndex + 1);
+        setnextSongIndex((currentSongIndex + 1 > songs.length - 1) ? 0 : currentSongIndex + 1)
     }, [currentSongIndex, songs.length])
 
     useEffect(() => {
         if (isplaying)
-            audioEl.current.play();
-        else audioEl.current.pause();
+            audioEl.current.play()
+        else audioEl.current.pause()
     })
 
     const skipSong = (forwards = true) => {
-        if (forwards) {
-            setCurrentSongIndex(() => (currentSongIndex + 1 > songs.length - 1) ? 0 : currentSongIndex + 1);
-        } else {
-            setCurrentSongIndex(() => (currentSongIndex - 1 < 0) ? songs.length - 1 : currentSongIndex - 1);
+        if (forwards)
+            setCurrentSongIndex(() => (currentSongIndex + 1 > songs.length - 1) ? 0 : currentSongIndex + 1)
+        else setCurrentSongIndex(() => (currentSongIndex - 1 < 0) ? songs.length - 1 : currentSongIndex - 1)
+    }
+
+    const style = {
+        nowPlaying: {
+            fontSize: 16,
+            textAlign: 'center',
+            marginTop: 8,
+            marginBottom: 8,
+            textTransform: 'uppercase',
+        },
+        nextUp: {
+            fontSize: 16,
+            textAlign: 'center',
+            marginTop: 8,
+            marginBottom: 8,
+        },
+        signOut: {
+            position: 'absolute',
+            right: 8,
+            top: 8,
         }
     }
 
     return (
-        <div className='player'>
-            <button onClick={() => setAuthtoken(null)}>Sign out</button>
-            <audio src={songs[currentSongIndex].src} ref={audioEl}></audio>
-            <h4>Now Playing</h4>
-            <PlayerDetails
-                {...songs[currentSongIndex]}
-            />
-            <PlayerControls
-                isplaying={isplaying}
-                setIsplaying={setIsplaying}
-                skipSong={skipSong}
-            />
-            <p><strong>Next up:</strong> {songs[nextSongIndex].title} by {songs[nextSongIndex].artist}</p>
-        </div>
+        <Grid container component='main' sx={{ height: '100vh' }}>
+            <CssBaseline />
+            <Grid item xs={false} sm={1} md={2} lg={3} />
+            <Grid item xs={12} sm={10} md={8} lg={6} backgroundColor='secondary' sx={{ position: 'relative', paddingTop: 5 }}>
+                <Button style={style.signOut} size='small' onClick={() => setAuthtoken(null)} variant='contained'>Sign out</Button>
+                <audio src={songs[currentSongIndex].src} ref={audioEl}></audio>
+                <Typography style={style.nowPlaying} component='h4'>Now Playing</Typography>
+                <PlayerDetails
+                    {...songs[currentSongIndex]}
+                />
+                <PlayerControls
+                    isplaying={isplaying}
+                    setIsplaying={setIsplaying}
+                    skipSong={skipSong}
+                />
+                <Typography style={style.nextUp}>
+                    <strong>Next up:</strong> {songs[nextSongIndex].title} <em>by</em> {songs[nextSongIndex].artist}
+                </Typography>
+            </Grid>
+            <Grid item xs={false} sm={1} md={2} lg={3} />
+        </Grid>
     )
 }
